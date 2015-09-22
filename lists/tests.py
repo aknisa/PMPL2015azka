3031316ebf5615e72613f59b8012d81d620755c7
aknisa <azka.khairun@gmail.com>
2015-09-22 21:37:35 +0700

    new URL + view for adding to existing lists. FT passes :-)

--- lists/tests.py
+++ lists/tests.py
@@ -31,36 +31,7 @@ class HomePageTest(TestCase):
         #self.assertIn('itemey 1', response.content.decode())
         #self.assertIn('itemey 2', response.content.decode())
 
-    def test_home_page_displays_comment_if_todolist_is_empty(self):
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(),0)
-        self.assertIn('yey, waktunya berlibur', response.content.decode())
-
-    def test_home_page_displays_comment_if_todolist_is_less_than_5(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='Itemey 1', list=list_)
-        
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertLess(Item.objects.count(),5)
-        self.assertIn('sibuk tapi santai', response.content.decode())
-
-    def test_home_page_displays_comment_if_todolist_is_greater_equal_than_5(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='Itemey 1', list=list_)
-        Item.objects.create(text='Itemey 2', list=list_)
-        Item.objects.create(text='Itemey 3', list=list_)
-        Item.objects.create(text='Itemey 4', list=list_)
-        Item.objects.create(text='Itemey 5', list=list_)
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertGreaterEqual(Item.objects.count(), 5)
-        self.assertIn('oh tidak', response.content.decode())
+    
         
 class ListAndItemModelsTest(TestCase):
 
@@ -95,18 +66,66 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
+
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+    
+    def test_home_page_displays_comment_if_todolist_is_empty(self):
+        request = HttpRequest()
+        response = home_page(request)
+        #response = self.client.get('/lists/the-only-list-in-the-world/') #1
+
+        self.assertEqual(Item.objects.count(),0)
+        #self.assertIn('yey, waktunya berlibur', response.content.decode())
+        self.assertContains(response, 'yey, waktunya berlibur')
+
+    def test_home_page_displays_comment_if_todolist_is_less_than_5(self):
         list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+        Item.objects.create(text='Itemey 1', list=list_)
+        
+        request = HttpRequest()
+        response = home_page(request)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/') #1
+        self.assertLess(Item.objects.count(),5)
+        #self.assertIn('sibuk tapi santai', response.content.decode())
+        self.assertContains(response, 'sibuk tapi santai')
 
-        self.assertContains(response, 'itemey 1') #2
-        self.assertContains(response, 'itemey 2') #3
+    def test_home_page_displays_comment_if_todolist_is_greater_equal_than_5(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='Itemey 1', list=list_)
+        Item.objects.create(text='Itemey 2', list=list_)
+        Item.objects.create(text='Itemey 3', list=list_)
+        Item.objects.create(text='Itemey 4', list=list_)
+        Item.objects.create(text='Itemey 5', list=list_)
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertGreaterEqual(Item.objects.count(), 5)
+        #self.assertIn('oh tidak', response.content.decode())
+        self.assertContains(response, 'oh tidak')
 
 class NewListTest(TestCase):
 
@@ -125,4 +144,33 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
