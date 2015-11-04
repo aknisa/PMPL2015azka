3a221a25cff9b29de49284080f4959e979d540ca
aknisa <azka.khairun@gmail.com>
2015-11-04 18:41:36 +0700

    wsgi

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -16,6 +16,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 #application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
